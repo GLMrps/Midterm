77bc98df0218311009cbeecf448e58fc8daad64a
PinChangChung <130741403+PinChangChung@users.noreply.github.com>
2023-12-16 17:42:15 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,15 +6,13 @@ $(document).ready(function()
         {
             $("#bar1").attr("src","img/bar1-2.png");
             $("#bar2").attr("src","img/bar2-2.png");
+            $("#phone-img").style.display="block";
         }  
         if ($(window).width()>600)
         {
             $("#bar1").attr("src","img/img-bar1.png");
             $("#bar2").attr("src","img/img-bar2.png");
+            $("#phone-img").style.display="none";
         }
     });
-   $("#parent").hover(function()
-   {
-    $("#parent").click();
-   });
 });
