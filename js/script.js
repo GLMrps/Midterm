856a1f88b8aa32e0a096003a3a16f86e34a2af53
PinChangChung <130741403+PinChangChung@users.noreply.github.com>
2023-12-17 23:14:58 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,13 +6,25 @@ $(document).ready(function()
         {
             $("#bar1").attr("src","img/bar1-2.png");
             $("#bar2").attr("src","img/bar2-2.png");
+            $("#bar1-another").attr("src","../img/bar1-2.png");
+            $("#bar2-another").attr("src","../img/bar2-2.png");
+            $("#h-menu").removeClass("invisible");
+            $(".debug-cover").removeClass("invisible");
         }  
         if ($(window).width()>600)
         {
             $("#bar1").attr("src","img/img-bar1.png");
             $("#bar2").attr("src","img/img-bar2.png");
+            $("#bar1-another").attr("src","../img/img-bar1.png");
+            $("#bar2-another").attr("src","../img/img-bar2.png");
+            $("#h-menu").addClass("invisible");
+            $(".debug-cover").addClass("invisible");
         }
-
     });
 
+    $(".toggle").click(function()
+    {
+       $(".debug-cover").toggleClass("show");
+    })
+    
 });
