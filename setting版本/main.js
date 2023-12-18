2f3d8576abb8e82cf0ce833b4513aff6ce2899a6
Rayping <130742095+GLMrps@users.noreply.github.com>
2023-12-18 13:17:11 +0800

    Add files via upload

---
+++ setting版本/main.js
@@ -0,0 +1,48 @@
+var isGradient1 = true; // 初始漸層色為第一種
+var showIcon = document.getElementById('showIcon');
+var hideIcon = document.getElementById('hideIcon');
+
+
+document.getElementById('toggleGradientBtn').addEventListener('click', function() {
+
+
+    // 切換漸層色
+    isGradient1 = !isGradient1;
+    // 設置body的背景為新的漸層色
+    document.body.style.background = isGradient1 ? 'linear-gradient(to bottom, #000000, #461AC4)' : 'linear-gradient(to bottom, #8E92FF, #FFFFFF)';
+
+    
+});
+const stars = document.querySelectorAll('.star');
+
+/* setStarInitXPosition */
+
+const setStarMoveSpeed = (star) => {
+  const starMoveSpeed = -1 - Math.random();
+  star.dataset.speed = starMoveSpeed;
+};
+  var settingsButton = document.getElementById('settingsButton');
+  var settingsPanel = document.getElementById('settingsPanel');
+  var closeButton = document.getElementById('closeButton');
+
+      // 打开设置面板
+settingsButton.addEventListener('click', function() {
+    settingsPanel.style.display = 'block';
+});
+
+// 关闭设置面板
+closeButton.addEventListener('click', function() {
+    settingsPanel.style.display = 'none';
+});
+
+stars.forEach(setStarMoveSpeed);
+window.addEventListener('scroll', () => {
+    const scrollPositionY = window.pageYOffset;
+    const moon = document.querySelector('.moon');
+    //const settingsButton = document.querySelector('.settings-button');
+    //settingsButton.style.transform = `translateY(${scrollPositionY}px)`;
+    moon.style.transform = `translateY(${scrollPositionY}px)`;
+
+  });
+
+
